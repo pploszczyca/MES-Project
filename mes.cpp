64e325a38775c65789b476867468f31746937b19
Piotr Płoszczyca <piotrus.plo@gmail.com>
2021-01-09 15:53:41 +0100

    Modified solve2DMatrix function and added file with points for gnuplot

--- mes.cpp
+++ mes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <fstream>
 
 using namespace std;
 
@@ -35,7 +36,7 @@ class EiFunction{     // y(x) = a*x + b
 };
 
 const Interval omega(0,2);      // Przedział w którym szukamy funkcji
-const int integralAcc = 1000;       // Dokładność obliczania całki
+const int integralAcc = 50000;       // Dokładność obliczania całki
 
 int k(double x){
     if(x >= 0 && x <= 1)    return 1;
@@ -50,7 +51,9 @@ double calculateIntegral(EiFunction u, EiFunction v){       // Metoda kwadratów
 
     for(int i = 1; i <= integralAcc; i++){
         x = omega.from + i*h;
-        score += k(x)*u.derivative(x)*v.derivative(x)*h;
+        // score += k(x)*u.derivative(x)*v.derivative(x)*h;
+        score += u.derivative(x)*v.derivative(x)*h;
+
     }
 
     return score;
@@ -116,42 +119,57 @@ double ** makingMatrix(int n){
 
 double *solve2DMatrix(double **A, int n){       // Na podstawie algorytmu eliminacji Gaussa
     double *sollution = new double[n];
+    double tmp;
 
-    for(int i = 0; i < n; i++){
-        for(int j = i+1; j < n; j++){
-            if(abs(A[i][i]) < abs(A[j][i])){
-                for(int k = 0; k < n+1; k++){
-                    swap(A[i][k], A[j][k]);
-                }
-            }
-        }
-    }
+    for(int j = 0; j < n-1; j++){
+        for(int i = j+1; i < n; i++){
+            tmp = A[i][j]/A[j][j];
 
-    for(int i = 0; i < n-1; i++){
-        for(int j=i+1; j<n; j++){
-
-            double tmp = A[i][j]/A[i][i];
-            for(int k = 0; k < n+1; k++){
-                A[j][k] = A[j][k]-tmp*A[i][k];
+            for(int k =0; k < n+1; k++){
+                A[i][k] -= A[j][k]*tmp;
             }
         }
     }
 
     for(int i = n-1; i >= 0; i--){
-        sollution[i] = A[i][n];
-
-        for(int j = i+1; j < n; j++){
-            if(i != j){
-                sollution[i] = sollution[i] -A[i][j]*sollution[j];
-            }
-        }
-
-        sollution[i] = sollution[i]/A[i][i];
+        double s = 0;
+        for(int j = i+1; j < n; j++)
+            s += A[i][j] * sollution[j];
+        
+        sollution[i] = (A[i][n] - s)/A[i][i];
     }
-
+    
     return sollution;
 }
 
+double calculateXFromSollutionTab(double *sollution, int n, double x){
+    double score = 0;
+
+    for(int i = 0; i < n; i++){
+        score += sollution[i]*EiFunction(i,n).normal(x);
+    }
+
+    return score;
+}
+
+void makeFileToPlot(double *sollution, int n){
+    ofstream file("data.txt");
+
+    double h = (omega.to - omega.from)/(double) integralAcc;
+    double x;
+
+    for(int i = 1; i <= integralAcc; i++){
+        x = omega.from + i*h;
+        file.width(10);
+        file.fill(' ');
+        file << x;
+        file.width(10);
+        file.fill(' ');
+        file << calculateXFromSollutionTab(sollution, n, x) << endl;
+    }
+
+    file.close();
+}
 
 void calculatingSolution(int n){
     double **A = makingMatrix(n);
@@ -162,6 +180,8 @@ void calculatingSolution(int n){
     print2DMatrix(A, n);
     cout << endl;
     print1DMatrix(sollution, n);
+
+    makeFileToPlot(sollution, n);
 
     delete [] sollution;
     delete2DMatrix(A,n);
@@ -178,6 +198,7 @@ int main(){
     // cout << B(e1, e2) << endl;
     // cout << B(e2, e1) << endl;
     // cout << B(e2, e2) << endl;
+    cout << (double)0*(-1) << endl;
 
     calculatingSolution(n);
 
